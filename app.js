33f233cf0f4e6da15c5fb4c27e894a5f00b58dac
FaisalM <122456185+Faisal-Algabni@users.noreply.github.com>
2023-05-26 19:59:29 +0300

    Update DB

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3004
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Saleh')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+// 1st run this file then run DB 
