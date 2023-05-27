98f8fe93ce1cf90e67d898a8de9ab99d3fc3e2dc
FaisalM <122456185+Faisal-Algabni@users.noreply.github.com>
2023-05-27 11:18:33 +0300

    update server

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3004
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.send('Saleh')
+  res.sendFile("Home/index.html" , {root: __dirname})
+})
+
+
+//Error link 
+app.use((req,res) => {
+  res.status(404).send(" - Sorry can't find this page (404 Error) ")
 })
 
 app.listen(port, () => {
