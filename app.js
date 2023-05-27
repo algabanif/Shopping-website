a401f91024db40af5978d3e3a983a226939db478
FaisalM <122456185+Faisal-Algabni@users.noreply.github.com>
2023-05-27 17:36:23 +0300

    huge changes
    - Routing pages
    - Change html files to ejs files
    - create views and public folders
    - Arranges ejs files in public folder
    - Arranges css and js and images fin public folder
    - relink ejs files with css
    - styles posters and category in home pages

--- app.js
+++ app.js
@@ -2,28 +2,66 @@ const express = require('express')
 const app = express()
 const port = 3004
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 //for auto refresh 
 const path = require("path");
 const livereload = require("livereload");
 const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, 'pubplic'));
+liveReloadServer.watch(path.join(__dirname, 'public'));
 
 const connectLivereload = require("connect-livereload");
 app.use(connectLivereload());
 
-liveReloadServer.server.once("connection", () => { setTimeout(() => { liveReloadServer.refresh("/"); }, 100); })
+liveReloadServer.server.once("connection", () => { 
+  setTimeout(() => {
+    liveReloadServer.refresh("/"); 
+  }, 100); 
+});
 //End refresh
 
+
+
+
+//Routing 
 app.get('/', (req, res) => {
-  res.sendFile("Home/index.html" , {root: __dirname})
+  res.render("home")
 })
-
-
-//Error link 
-app.use((req,res) => {
+app.get('/home', (req, res) => {
+  res.render("home")
+})
+app.get('/index', (req, res) => {
+  res.render("home")
+})
+app.get('/Cart', (req, res) => {
+  res.render("Cart")
+})
+app.get('/cartEmpty', (req, res) => {
+  res.render("cartEmpty")
+})
+app.get('/category', (req, res) => {
+  res.render("category")
+})
+app.get('/Admin', (req, res) => {
+  res.render("Admin")
+})
+app.get('/profile', (req, res) => {
+  res.render("profile")
+})
+app.get('/services', (req, res) => {
+  res.render("services")
+})
+app.get('/signUp', (req, res) => {
+  res.render("signUp")
+})
+app.use((req,res) => { //Error link 
   res.status(404).send(" - Sorry can't find this page (404 Error) ")
 })
+//End Routing 
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
