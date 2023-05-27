2b3715c77b7e053d8edfae36ac9f7e5d77469f71
FaisalM <122456185+Faisal-Algabni@users.noreply.github.com>
2023-05-27 12:27:09 +0300

    Update server
    Establish live server for html and CSS by NodeJs

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const app = express()
 const port = 3004
 app.set('view engine', 'ejs')
 
+//for auto refresh 
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'pubplic'));
+
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => { setTimeout(() => { liveReloadServer.refresh("/"); }, 100); })
+//End refresh
+
 app.get('/', (req, res) => {
   res.sendFile("Home/index.html" , {root: __dirname})
 })
